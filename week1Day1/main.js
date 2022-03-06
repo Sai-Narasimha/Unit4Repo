52c24bb2abca5d2d9a2e104992ef0a20085e5fbd
Sai-Narasimha <95972105+Sai-Narasimha@users.noreply.github.com>
2022-03-06 17:12:33 +0530

    week1day1 assignment completed

---
+++ week1Day1/main.js
@@ -0,0 +1,2 @@
+const NewMovies = require("./movie");
+NewMovies()
---
+++ week1Day1/movie.js
@@ -0,0 +1,10 @@
+const movies =  {
+    movie1 : "Bheemla Nayak"
+}
+
+function newMovie(){
+    console.log(`Movies : ${movies.movie1}`)
+
+}
+module.exports = newMovie;
+// newMovie();
